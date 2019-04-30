3bdafa5a603463875a42b64e7d908b4f79931076
Taylor Arndt <taylorarndt@wireless79-079.wireless.wmich.edu>
2019-04-30 13:53:15 -0400

    added a button and documentation

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-from PyQt5.QtWidgets import QApplication, QLabel
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec_()
+# to run make sure you are in the right directory and type test.py you should now have a window.
+# imports the application, the layout, the controls like buttons, and the widgets.      
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,QLabel
 
+# inicializes the application, you only need 1 per app.
+ app = QApplication([])
+# makes a window for the application.
+ window = QWidget()
+# tells python we want to use the Vbox layout for our controls.
+ layout = QVBoxLayout()
+# adds a button. 
+layout.addWidget(QPushButton('this button should be accessible'))
+# adds a label. 
+layout.addWidget(QLabel('this is a label and i read correctly hello world!'))
+# sets the window layout.
+window.setLayout(layout)
+# shows the window on screen.
+window.show()
+# closes the app.
+ app.exec_()
 
